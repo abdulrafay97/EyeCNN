d2eaf395ba24c7e1604516a1f0b155968e2b3545
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2023-01-03 21:44:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ data_transform = transforms.Compose([
 if file_up is not None:
     # display image that user uploaded
     image = Image.open(file_up).convert('RGB')
-    st.image(image, caption = 'Uploaded Image.', use_column_width = False)
+    st.image(image, caption = 'Uploaded Image.', width=None, use_column_width=None)
     st.write("")
     st.write("Just a second ...")
     img = data_transform(image)
